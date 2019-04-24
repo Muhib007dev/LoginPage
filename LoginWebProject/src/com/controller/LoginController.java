e8f107ed3d39095a9e4898c0182d91eea56cc54e
Muhib007dev <muhib.shaikh313@gmail.com>
2019-04-24 15:38:43 +0530

    Adding

---
+++ LoginWebProject/src/com/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.service.User;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	
+	User login = null;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher disp = request.getRequestDispatcher("Login.html");
+		PrintWriter printWriter = response.getWriter();
+		String user = request.getParameter("unm");
+		String pss = request.getParameter("pwd");
+		
+		if(checkLogin(user,pss))
+			response.sendRedirect("Success.html");
+		else
+		{
+			disp.include(request, response);
+			printWriter.print("<b><br>Sorry try again with proper details</b>");
+		}
+	
+	}
+	public boolean checkLogin(String user,String pss){
+		System.out.println("Controller Layer...");
+		login = new User();
+		return login.getLogin(user,pss);
+	}
+
+}
---
+++ LoginWebProject/src/com/service/User.java
@@ -0,0 +1,54 @@
+package com.service;
+
+import java.util.List;
+
+import com.doalayer.LoginDao;
+
+public class User {
+	private String userName;
+	private String password;
+	LoginDao loginDao = null;
+	
+	public boolean getLogin(String user, String password){
+		System.out.println("service layer...");
+		List<User> userList = getUserData();
+
+		
+		for(User login:userList){
+			System.out.println(login.getUserName());
+			System.out.println(login.getPassword());
+			if(user.equals(login.getUserName()) && password.equals(login.getPassword()))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public List<User> getUserData(){
+		System.out.println("getUserData on service layer....");
+		loginDao = new LoginDao();
+		List<User> list = loginDao.getUser();
+		return list;
+	}
+	
+	public void setLoginData(String username, String password){
+		
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public void setPassword(String password){
+		this.password=password;
+	}
+	
+	public String getUserName(){
+		return userName;
+	}
+	
+	public void setUserName(String userName){
+		this.userName=userName;
+	}
+}
